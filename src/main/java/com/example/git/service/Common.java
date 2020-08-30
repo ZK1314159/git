0abc16f7ea143fa730d83d5f52fb784b7bae5fd7
ZK1314159 <979384911@qq.com>
2020-08-30 13:07:12 +0800

    add interface common

---
+++ src/main/java/com/example/git/service/Common.java
@@ -0,0 +1,12 @@
+package com.example.git.service;
+
+/**
+ * Description：<br>
+ *
+ * @author zeng.kai <br>
+ * CreateDate：2020/8/30 13:06 <br>
+ */
+public interface Common {
+
+    void test();
+}
